6c4706eb17ba4adc0be160bf35c0a6d0cb31dafb
Emilio Peláez <me@epelaez.net>
2023-10-12 00:20:36 +0200

    Watch App

---
+++ Example/HierarchyWatch Watch App/HierarchyWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HierarchyWatchApp.swift
+//  HierarchyWatch Watch App
+//
+//  Created by Emilio Peláez on 14/9/23.
+//
+
+import SwiftUI
+
+@main
+struct HierarchyWatch_Watch_AppApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
