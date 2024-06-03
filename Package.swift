ad212a46201d082edec9d3747cd60639143006ad
Emilio Peláez <me@epelaez.net>
2024-06-04 00:48:06 +0200

    Added visionOS Platforms and bumped Swift Tools

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
 	name: "HierarchyResponder",
-	platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v6), .macCatalyst(.v13), .tvOS(.v13)],
+	platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v6), .macCatalyst(.v13), .tvOS(.v13), .visionOS(.v1)],
 	products: [
 		.library(
 			name: "HierarchyResponder",
